48e73455a87a98e4e87b851612ee37e9cb1e1979
Sam Foster <slfotg@gmail.com>
2024-08-26 08:41:55 -0500

    Added context menu to search selected text

--- extension.js
+++ extension.js
@@ -1,44 +1,64 @@
 const vscode = require('vscode');
 const axios = require('axios');
 
+const oeis = {
+	url: 'https://oeis.org',
+	searchUrl: 'https://oeis.org/search',
+}
+
+async function showResults(results, value) {
+	const item = await vscode.window.showQuickPick(results, {
+		title: `Search results ${value}`,
+		ignoreFocusOut: true,
+		matchOnDescription: true,
+		matchOnDetail: true,
+	});
+	if (!item) return;
+	const url = `${oeis.url}/${item.label}`;
+	vscode.env.openExternal(vscode.Uri.parse(url));
+}
+
+async function searchOeis(value) {
+	console.log(value);
+	const response = await axios.get(oeis.searchUrl, { params: { q: value, fmt: 'json' } });
+	console.log(response);
+
+	const results = response.data.results.map(({ number, data, name }) => {
+		const seqId = "A" + number.toString().padStart(6, '0');
+		return {
+			label: seqId,
+			description: name,
+			detail: `[${data}]`,
+		}
+	});
+	console.log(results);
+
+	showResults(results, value);
+}
+
 /**
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
 
-	const disposable = vscode.commands.registerCommand('oeis.search', function () {
-
+	const oeisSearch = vscode.commands.registerCommand('oeis.search', function () {
 		vscode.window.showInputBox({
 			placeHolder: "1,1,2,3,5,8",
 		}).then(value => {
-
-			axios.get('https://oeis.org/search', { params: { q: value, fmt: 'json' } })
-				.then(response => {
-
-					const results = response.data.results.map(({ number, data, name }) => {
-						const seqId = "A" + number.toString().padStart(6, '0');
-						return {
-							label: seqId,
-							description: name,
-							detail: `[${data}]`,
-						}
-					});
-
-					vscode.window.showQuickPick(results, {
-						title: `Search results ${value}`,
-					}).then(item => {
-						if (!item) return;
-						const url = `https://oeis.org/${item.label}`;
-						vscode.env.openExternal(vscode.Uri.parse(url));
-					});
-				})
-				.catch(error => {
-					console.error(error);
-				});
+			searchOeis(value);
 		});
 	});
 
-	context.subscriptions.push(disposable);
+	const oeisSearchSelected = vscode.commands.registerCommand('oeis.searchSelected', function () {
+		const editor = vscode.window.activeTextEditor;
+		if (!editor) return;
+
+		const selection = editor.selection;
+		const value = editor.document.getText(selection);
+		searchOeis(value);
+	});
+
+	context.subscriptions.push(oeisSearch);
 }
 
 function deactivate() { }
