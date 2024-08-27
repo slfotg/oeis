3df0f5d9411a0115fe557c12d951efba521a2069
Sam Foster <slfotg@gmail.com>
2024-08-26 23:46:23 -0500

    Added sequence information within vscode

--- README.md
+++ README.md
@@ -1,10 +1,32 @@
 # OEIS
 
-Allows basic searching of OEIS from VSCode
+Allows basic searching of [OEIS](oeis.org) from VSCode
 
 ## Features
 
-Allows searching for sequences in OEIS by running the `oeis.search` command. A quick pick menu will show potential matches and clicking one of them will open that sequence in OEIS in your browser.
+Allows searching for sequences in OEIS by running the `oeis.search` command:
+
+![command](images/command.png)
+
+View results of your search:
+
+![results](images/results.png)
+
+Filter results of your search:
+
+![filtered](images/filtered_results.png)
+
+Show sequence information in a new tab:
+
+![display](images/display.png)
+
+Highlight and right-click allows a quick search for a sequence from your editor.
+
+![right_click](images/right_click.png)
+
+![right_click](images/right_click.gif)
+
+Links to [OEIS](oeis.org) will also appear in the description to allow you to quickly navigate to the website.
 
 ## Usage
 
@@ -12,6 +34,6 @@ Hit Ctrl+Shift+P to open command palette and search for "OEIS".
 
 After selecting the "OEIS: Sequence Search" command, just type any sequence like you would on the oeis website.
 
-If any sequences are found, they will be displayed in a drop down with basic information. When you select one of those items, you will be brought to the OEIS page on that sequence in your browser.
+If any sequences are found, they will be displayed in a drop down with basic information. When you select one of those items, its description will open in a new window.
 
 I recommend creating a key binding for the command. I'm currently using Ctrl+Alt+S.
--- extension.js
+++ extension.js
@@ -4,9 +4,114 @@ const axios = require('axios');
 const oeis = {
 	url: 'https://oeis.org',
 	searchUrl: 'https://oeis.org/search',
+	sections: [
+		['offset', 'Offset'],
+		['comment', 'Comments'],
+		['reference', 'References'],
+		['link', 'Links'],
+		['formula', 'Formula'],
+		['example', 'Example'],
+		['maple', 'Maple'],
+		['mathematica', 'Mathematica'],
+		['program', 'Prog'],
+		['xref', 'Crossrefs'],
+		['keyword', 'Keywords'],
+		['author', 'Author']
+	]
 }
 
-async function showResults(results, value) {
+function splitData(data) {
+	return data.split(',').join(', ');
+}
+
+function splitLines(lines) {
+	if (typeof lines === 'string') return `<div class="seq"><tt>${lines}</tt></div>`;
+	return lines.map(line => `<div class="seq"><tt>${line}</tt></div>`).join('\n');
+}
+
+function displaySection(key, label, data) {
+	const lines = data[key];
+	if (!lines) return '';
+	return `<br />
+			<div class="section">
+				<div class="label">
+					<tt>${label}</tt>
+				</div>
+				<div class="description">
+					<tt>${splitLines(lines)}</tt>
+				</div>
+			</div>`
+}
+
+/**
+ * @param {vscode.ExtensionContext} context
+ */
+function getHtml(label, context) {
+	const response = context.workspaceState.get(label);
+	const sections = oeis.sections.map(([key, label]) => displaySection(key, label, response)).join('\n');
+	return `<!DOCTYPE html>
+			<html lang="en">
+			<head>
+				<meta charset="UTF-8">
+				<meta name="viewport" content="width=device-width, initial-scale=1.0">
+				<style lang="css">
+					body {
+						padding-left: 30px;
+					}
+					hr {
+						width: 100%;
+					}
+					.content {
+						display: flex;
+						flex-direction: column;
+						justify-content: center;
+						align-items: center;
+						width: 60%;
+					}
+					.data {
+						gap: 10px;
+						width: 95%;
+					}
+					.section {
+						display: flex;
+						flex-direction: row;
+						gap: 10px;
+						width: 90%;
+					}
+					.label {
+						font-weight: bold;
+						order: 1;
+						width: 10%;
+						border: 1px;
+					}
+					.description {
+						order: 2;
+						width: 90%;
+					}
+					div.seq tt {
+						text-indent: 3em hanging each-line;
+					}
+				</style>
+				<title>${label}</title>
+			</head>
+			<body>
+				<div class="content">
+					<h2><tt><a href="${oeis.url}/${label}">${label}</a> - ${response.name}</tt></h2>
+					<hr />
+					<div class="data">
+						<tt>${splitData(response.data)}</tt></td>
+					</div>
+					<hr />
+					${sections}
+				</div>
+			</body>
+			</html>`;
+}
+
+/**
+ * @param {vscode.ExtensionContext} context
+ */
+async function showResults(results, value, context) {
 	const item = await vscode.window.showQuickPick(results, {
 		title: `Search results ${value}`,
 		ignoreFocusOut: true,
@@ -15,10 +120,15 @@ async function showResults(results, value) {
 	});
 	if (!item) return;
 	const url = `${oeis.url}/${item.label}`;
-	vscode.env.openExternal(vscode.Uri.parse(url));
+	var panel = vscode.window.createWebviewPanel('oeis', item.label, vscode.ViewColumn.One, { enableScripts: true });
+	const html = getHtml(item.label, context);
+	panel.webview.html = html;
 }
 
-async function searchOeis(value) {
+/**
+ * @param {vscode.ExtensionContext} context
+ */
+async function searchOeis(value, context) {
 	const response = await axios.get(oeis.searchUrl, { params: { q: value, fmt: 'json' } });
 
 	const results = response.data.results.map(({ number, data, name }) => {
@@ -30,19 +140,23 @@ async function searchOeis(value) {
 		}
 	});
 
-	showResults(results, value);
+	for (const result of response.data.results) {
+		const seqId = "A" + result.number.toString().padStart(6, '0');
+		context.workspaceState.update(seqId, result);
+	}
+
+	showResults(results, value, context);
 }
 
 /**
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
-
 	const oeisSearch = vscode.commands.registerCommand('oeis.search', function () {
 		vscode.window.showInputBox({
 			placeHolder: "1,1,2,3,5,8",
 		}).then(value => {
-			searchOeis(value);
+			searchOeis(value, context);
 		});
 	});
 
@@ -52,10 +166,11 @@ function activate(context) {
 
 		const selection = editor.selection;
 		const value = editor.document.getText(selection);
-		searchOeis(value);
+		searchOeis(value, context);
 	});
 
 	context.subscriptions.push(oeisSearch);
+	context.subscriptions.push(oeisSearchSelected);
 }
 
 function deactivate() { }
